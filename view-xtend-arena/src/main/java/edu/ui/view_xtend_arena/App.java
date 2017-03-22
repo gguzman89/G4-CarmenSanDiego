690fee783e4193965bcf463721650e84ab5d1d5e
gguzman89 <ggtpi0gabriel@gmail.com>
2017-03-22 14:13:13 -0300

    add proyecto view mas config arena

---
+++ view-xtend-arena/src/main/java/edu/ui/view_xtend_arena/App.java
@@ -0,0 +1,13 @@
+package edu.ui.view_xtend_arena;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
