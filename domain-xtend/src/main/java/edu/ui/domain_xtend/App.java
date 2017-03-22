af88ffdfa0cb8a3fd16f99a670f1efcb491da29b
gguzman89 <ggtpi0gabriel@gmail.com>
2017-03-22 12:40:52 -0300

    domain y gitignore

---
+++ domain-xtend/src/main/java/edu/ui/domain_xtend/App.java
@@ -0,0 +1,13 @@
+package edu.ui.domain_xtend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
