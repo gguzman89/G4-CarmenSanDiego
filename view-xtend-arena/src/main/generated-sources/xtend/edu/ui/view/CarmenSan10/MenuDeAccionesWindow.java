d1aaff716b61937fada4231486aa2a81a6b5e882
Alan Rodriguez <alan.nicolas.rodriguez@gmail.com>
2017-04-09 21:01:19 -0300

    intento de ventana principal 1

---
+++ view-xtend-arena/src/main/generated-sources/xtend/edu/ui/view/CarmenSan10/MenuDeAccionesWindow.java
@@ -0,0 +1,68 @@
+package edu.ui.view.CarmenSan10;
+
+import edu.ui.domain.CarmenSan10.Detective;
+import org.eclipse.xtext.xbase.lib.ObjectExtensions;
+import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.uqbar.arena.layout.HorizontalLayout;
+import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Label;
+import org.uqbar.arena.widgets.Panel;
+import org.uqbar.arena.windows.SimpleWindow;
+import org.uqbar.arena.windows.WindowOwner;
+
+@SuppressWarnings("all")
+public class MenuDeAccionesWindow extends SimpleWindow<Detective> {
+  /**
+   * esta es la primer ventana que me aparece cuando inicio el juego,
+   * la hice extender de detective, ya que son comportamientos basicos de un detective
+   */
+  public MenuDeAccionesWindow(final WindowOwner parent) {
+    super(parent, new Detective());
+  }
+  
+  public void createMainTemplate(final Panel mainPanel) {
+    this.setTitle("¿Donde esta Carmen Sandiego?");
+  }
+  
+  protected void createFormPanel(final Panel mainPanel) {
+    Panel _panel = new Panel(mainPanel);
+    final Procedure1<Panel> _function = new Procedure1<Panel>() {
+      public void apply(final Panel it) {
+        HorizontalLayout _horizontalLayout = new HorizontalLayout();
+        it.setLayout(_horizontalLayout);
+      }
+    };
+    final Panel panelDelFormulario = ObjectExtensions.<Panel>operator_doubleArrow(_panel, _function);
+    Label _label = new Label(panelDelFormulario);
+    final Procedure1<Label> _function_1 = new Procedure1<Label>() {
+      public void apply(final Label it) {
+        it.setText("¿Que haremos ahora Detective?");
+      }
+    };
+    ObjectExtensions.<Label>operator_doubleArrow(_label, _function_1);
+  }
+  
+  protected void addActions(final Panel actionsPanel) {
+    Button _button = new Button(actionsPanel);
+    final Procedure1<Button> _function = new Procedure1<Button>() {
+      public void apply(final Button it) {
+        it.setCaption("Resolver Misterio");
+      }
+    };
+    ObjectExtensions.<Button>operator_doubleArrow(_button, _function);
+    Button _button_1 = new Button(actionsPanel);
+    final Procedure1<Button> _function_1 = new Procedure1<Button>() {
+      public void apply(final Button it) {
+        it.setCaption("Mapamundi");
+      }
+    };
+    ObjectExtensions.<Button>operator_doubleArrow(_button_1, _function_1);
+    Button _button_2 = new Button(actionsPanel);
+    final Procedure1<Button> _function_2 = new Procedure1<Button>() {
+      public void apply(final Button it) {
+        it.setCaption("Expedientes");
+      }
+    };
+    ObjectExtensions.<Button>operator_doubleArrow(_button_2, _function_2);
+  }
+}
--- view-xtend-arena/src/main/java/edu/ui/view/runnable/CarmenApplication.xtend
+++ view-xtend-arena/src/main/java/edu/ui/view/runnable/CarmenApplication.xtend
@@ -3,13 +3,22 @@ package edu.ui.view.runnable
 import org.uqbar.arena.Application
 import edu.ui.view.CarmenSan10.MapamundiWindow
 import edu.ui.domain.CarmenSan10.Mapamundi
+import edu.ui.domain.CarmenSan10.Detective
+import edu.ui.view.CarmenSan10.MenuDeAccionesWindow
+import org.uqbar.arena.windows.Window
 
 class CarmenApplication extends Application{
 	
 	override createMainWindow() {
-		new MapamundiWindow(this, new Mapamundi)
+		//new MapamundiWindow(this, new Mapamundi)
+		new MenuDeAccionesWindow(this)
 	}
 	
+ 
+	
+/*	override protected Window<?> createMainWindow() {
+		return new MenuDeAccionesWindow(this)
+	}*/
 	/**
 	 * cada ves que quieran probar su(s) ventana(s) vengan y cambien aqui
 	 * tanto en el createMainWindow como en main y denle run en .star
