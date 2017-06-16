20fbcfe1c4facd64949735a62f10e5308ee53199
gguzman89 <ggtpi0gabriel@gmail.com>
2017-06-16 15:32:26 -0300

    view ordenDeArresto

---
+++ CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/OrdenDeArrestoActivity.java
@@ -0,0 +1,13 @@
+package com.example.nigthkids.carmenmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrdenDeArrestoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orden_de_arresto);
+    }
+}
