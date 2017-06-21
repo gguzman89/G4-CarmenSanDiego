81dc6881e9a29ca927d3d0f7b8ccf82b8a5fd445
gguzman89 <ggtpi0gabriel@gmail.com>
2017-06-21 20:57:48 -0300

    jasdfkjas

--- CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/PedirPistasActivity.java
+++ CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/PedirPistasActivity.java
@@ -3,11 +3,31 @@ package com.example.nigthkids.carmenmobile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.nigthkids.carmenmobile.service.CarmenService;
+
+import retrofit.RestAdapter;
+
 public class PedirPistasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedir_pistas);
+
+        getSupportActionBar().setTitle("sadsadas");
+
+        iniciarCamenService();
+    }
+
+    private CarmenService iniciarCamenService() {
+
+        String SERVER_IP = "10.9.1.115";
+        String API_URL = "http://" + SERVER_IP + ":9000";
+
+        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(API_URL).build();
+
+        CarmenService carmenService = restAdapter.create(CarmenService.class);
+
+        return carmenService;
     }
 }
