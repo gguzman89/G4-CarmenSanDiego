fc6ad55702e03b4aba1bfaf2ab617bafa13010b5
LizaChambi <liza.chambi@gmail.com>
2017-06-27 12:14:54 -0300

    navegacion de ViajarActivity

--- CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/ViajarActivity.java
+++ CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/ViajarActivity.java
@@ -1,13 +1,35 @@
 package com.example.nigthkids.carmenmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViajarActivity extends AppCompatActivity {
+
+    Button orden;
+    Button pistas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viajar);
+
+        orden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ordenView = new Intent(ViajarActivity.this, OrdenDeArrestoActivity.class);
+                startActivity(ordenView);
+            }
+        });
+
+        pistas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent lugaresView = new Intent(ViajarActivity.this, OrdenDeArrestoActivity.class);
+                startActivity(lugaresView);
+            }
+        });
     }
 }
