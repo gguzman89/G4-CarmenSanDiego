d4cc810f7288207ff56f21f29df926533add1f84
gguzman89 <ggtpi0gabriel@gmail.com>
2017-06-27 12:22:37 -0300

    init ViajarActivity

--- CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/ViajarActivity.java
+++ CarmenMobile/app/src/main/java/com/example/nigthkids/carmenmobile/ViajarActivity.java
@@ -2,12 +2,25 @@ package com.example.nigthkids.carmenmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ViajarActivity extends AppCompatActivity {
+
+    ListView lvPaises;
+
+    String[] items = {"Argentina", "Bolivia", "Paraguay", "Chile"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viajar);
+
+        lvPaises = (ListView) findViewById(R.id.lvPaises);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1, items);
+        lvPaises.setAdapter(adapter);
     }
 }
